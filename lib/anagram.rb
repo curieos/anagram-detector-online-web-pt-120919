ee39895d253d6bb00423db84abaa3642d07ed6e7
Caroline Longnecker <cvrieous@gmail.com>
2020-01-07 21:55:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,9 +9,9 @@ class Anagram
   def match(words)
     collection = []
     words.each do |word|
-      puts @word.split("/S").sort
-      puts word.split("/S").sort
-      if @word.split("/S").sort == word.split("/S").sort
+      puts @word.split("").sort
+      puts word.split("").sort
+      if @word.split("").sort == word.split("").sort
         collection << word
       end
     end
